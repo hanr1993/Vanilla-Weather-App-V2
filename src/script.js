124663dbf6605004684077b0612e3174cea37d8d
hanr1993 <92591105+hanr1993@users.noreply.github.com>
2021-12-10 16:43:21 +0000

    Updated the days of week

--- src/script.js
+++ src/script.js
@@ -24,15 +24,7 @@ function formatDate(timestamp) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   return days[day];
 }
